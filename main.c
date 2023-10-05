528eda0d42ccd7d9226ec16c580a70e834f6bb9e
kipkirui73 <146317065+kipkirui73@users.noreply.github.com>
2023-10-05 09:26:10 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (){
+int a,b,c, d;
+printf("Enter first number: ");
+scanf("%d", &a);
+
+printf("Enter second number: ");
+scanf("%d", &b);
+
+printf("Enter third number: ");
+scanf("%d", &c);
+
+d = a + b + c;
+printf("%d + %d + %d = %d\n", a, b, c, d);
+
+
+return 0;}
+
+
